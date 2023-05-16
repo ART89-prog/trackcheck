d272ace4731b935c00c9118009c700707b5444c2
ART89-prog <dykpgilkom@mail.ru>
2023-05-16 23:31:03 +0300

    16553651651

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,46 @@
 $(() => {
 
 
-    // Ширина окна для ресайза
-    WW = window.innerWidth || document.clientWidth || document.getElementsByTagName('body')[0].clientWidth
-    WH = window.innerHeight || document.clientHeight || document.getElementsByTagName('body')[0].clientHeight
-    BODY = document.getElementsByTagName('body')[0]
-    OVERLAY = document.querySelector('.overlay')
+	// Ширина окна для ресайза
+	WW = window.innerWidth || document.clientWidth || document.getElementsByTagName('body')[0].clientWidth
+	WH = window.innerHeight || document.clientHeight || document.getElementsByTagName('body')[0].clientHeight
+	BODY = document.getElementsByTagName('body')[0]
+	OVERLAY = document.querySelector('.overlay')
 
 
-    // Моб. меню
-    $('header .mob_menu_btn').click((e) => {
-        e.preventDefault()
+	// Моб. меню
+	$('header .mob_menu_btn').click((e) => {
+		e.preventDefault()
 
-        $('header .mob_menu_btn').addClass('active')
-        $('body').addClass('menu_open')
-        $('header .menu').addClass('show')
-        $('.overlay').fadeIn(300)
-    })
+		$('header .mob_menu_btn').addClass('active')
+		$('body').addClass('menu_open')
+		$('header .menu').addClass('show')
+		$('.overlay').fadeIn(300)
+	})
 
-    $('header .close_btn, header .menu .item a, .overlay').click((e) => {
-        e.preventDefault()
+	$('header .close_btn, header .menu .item a, .overlay').click((e) => {
+		e.preventDefault()
 
-        $('header .mob_menu_btn').removeClass('active')
-        $('body').removeClass('menu_open')
-        $('header .menu').removeClass('show')
-        $('.overlay').fadeOut(300)
-    })
-
-
-
-    $('body').on('click', '.modal_link', function (e) {
-      e.preventDefault()
-
-      Fancybox.close(true)
-        Fancybox.show([{
-            src: $(this).data('content'),
-            type: 'inline',
-        }]);
-  })
+		$('header .mob_menu_btn').removeClass('active')
+		$('body').removeClass('menu_open')
+		$('header .menu').removeClass('show')
+		$('.overlay').fadeOut(300)
+	})
 
 
-    	// Fancybox
+
+	$('body').on('click', '.modal_link', function (e) {
+		e.preventDefault()
+
+		Fancybox.close(true)
+		Fancybox.show([{
+			src: $(this).data('content'),
+			type: 'inline',
+		}]);
+	})
+
+
+	// Fancybox
 	Fancybox.defaults.autoFocus = false
 	Fancybox.defaults.trapFocus = false
 	Fancybox.defaults.dragToClose = false
@@ -59,9 +59,9 @@ $(() => {
 	// }
 
 
-  $('input[type=tel]').inputmask('+7 (999) 999-99-99')
+	$('input[type=tel]').inputmask('+7 (999) 999-99-99')
 
-  // Табы
+	// Табы
 	var locationHash = window.location.hash
 
 	$('body').on('click', '.tabs button', function (e) {
@@ -97,7 +97,7 @@ $(() => {
 	}
 
 
-  	// Аккордион
+	// Аккордион
 	$('body').on('click', '.accordion .accordion_item .head', function (e) {
 		e.preventDefault()
 
@@ -114,54 +114,126 @@ $(() => {
 		}
 	})
 
-  	// Скрол к пунктам меню
-	$(".scroll").on("click", function(e){
+	// Скрол к пунктам меню
+	$(".scroll").on("click", function (e) {
 		e.preventDefault();
 		let id = $(this).attr("href");
 
 		$("html, body").animate({
-				scrollTop: $(id).offset().top - 50
-			}, {
-				duration: 1500,
-				easing: "swing"
+			scrollTop: $(id).offset().top - 50
+		}, {
+			duration: 1500,
+			easing: "swing"
 		});
 	});
 
 
 
-   
-    
 
-   window.addEventListener('resize', function () {
-    WH = window.innerHeight || document.clientHeight || document.getElementsByTagName('body')[0].clientHeight
-  
-    let windowW = window.outerWidth
-  
-    if (typeof WW !== 'undefined' && WW != windowW) {
-  
-  
-      // Перезапись ширины окна
-      WW = window.innerWidth || document.clientWidth || document.getElementsByTagName('body')[0].clientWidth
-  
-  
-      // Моб. версия
-      if (!fakeResize) {
-        fakeResize = true
-        fakeResize2 = false
-  
-        document.getElementsByTagName('meta')['viewport'].content = 'width=device-width, initial-scale=1, maximum-scale=1'
-      }
-  
-      if (!fakeResize2) {
-        fakeResize2 = true
-  
-        if (windowW < 375) document.getElementsByTagName('meta')['viewport'].content = 'width=375, user-scalable=no'
-      } else {
-        fakeResize = false
-        fakeResize2 = true
-      }
-    }
-  })
+	// const swiper = new Swiper('.swiper-container', {
+	// 	slidesPerView: 7,
+	// 	spaceBetween: 42,
+	// 	navigation: {
+	// 		nextEl: '.swiper-button-next',
+	// 		prevEl: '.swiper-button-prev',
+	// 	},
+	// 	breakpoints: {
+	// 		320: {
+	// 			slidesPerView: 2,
+	// 			spaceBetween: 20
+	// 		},
+	// 		480: {
+	// 			slidesPerView: 3,
+	// 			spaceBetween: 30
+	// 		},
+	// 		640: {
+	// 			slidesPerView: 6,
+	// 			spaceBetween: 42
+	// 		}
+	// 	}
+	// })
+
+
+
+	const partnerSliders = [],
+	partner = document.querySelectorAll('.partners .swiper-container')
+
+	partner.forEach(function (el, i) {
+		el.classList.add('partner_s' + i)
+
+		let options = {
+			loop: true,
+			speed: 500,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			preloadImages: false,
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},			
+
+			breakpoints: {
+				0: {
+					spaceBetween: 10,
+					slidesPerView: 1
+				},
+				480: {
+					spaceBetween: 10,
+					slidesPerView: 2
+				},
+				768: {
+					spaceBetween: 14,
+					slidesPerView: 3
+				},
+				1023: {
+					spaceBetween: 14,
+					slidesPerView: 4
+				},
+				1280: {
+					spaceBetween: 10,
+					slidesPerView: 5
+				}
+			}
+		}
+
+		partnerSliders.push(new Swiper('.partner_s' + i, options))
+	})
+
+
+
+
+
+	window.addEventListener('resize', function () {
+		WH = window.innerHeight || document.clientHeight || document.getElementsByTagName('body')[0].clientHeight
+
+		let windowW = window.outerWidth
+
+		if (typeof WW !== 'undefined' && WW != windowW) {
+
+
+			// Перезапись ширины окна
+			WW = window.innerWidth || document.clientWidth || document.getElementsByTagName('body')[0].clientWidth
+
+
+			// Моб. версия
+			if (!fakeResize) {
+				fakeResize = true
+				fakeResize2 = false
+
+				document.getElementsByTagName('meta')['viewport'].content = 'width=device-width, initial-scale=1, maximum-scale=1'
+			}
+
+			if (!fakeResize2) {
+				fakeResize2 = true
+
+				if (windowW < 375) document.getElementsByTagName('meta')['viewport'].content = 'width=375, user-scalable=no'
+			} else {
+				fakeResize = false
+				fakeResize2 = true
+			}
+		}
+	})
 
 
 
